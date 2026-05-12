2b53422ba1869f37f1f668c885100d2da480a391
abhinavsaxena2308 <abhinav.exam.2003@gmail.com>
2026-05-12 13:16:17 +0530

    card count changed

--- src/pages/Cars.tsx
+++ src/pages/Cars.tsx
@@ -87,7 +87,7 @@ const Cars: React.FC = () => {
 
         {/* Cars Grid */}
         {filteredCars.length > 0 ? (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
             {filteredCars.map((car, i) => (
               <CarCard key={car.id} car={car} index={i} />
             ))}
